87677668919b07ce7d595451c2b25ca066c539f8
Helder45 <helder.pereira@estudante.ifms.edu.br>
2024-12-20 08:51:51 -0400

    generate model and controller: Product

---
+++ db/migrate/20241220125033_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :tittle
+      t.string :description
+      t.string :brand
+      t.float :value
+      t.integer :size
+      t.integer :quantity
+      t.references :Category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
