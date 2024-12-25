4a94cdadb0a598501ac1eaf8be927a49373f95f2
Helder45 <helder.pereira@estudante.ifms.edu.br>
2024-12-25 13:08:24 -0400

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_18_001130) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_25_164411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,19 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_18_001130) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "tittle"
+    t.string "description"
+    t.string "brand"
+    t.float "value"
+    t.integer "size"
+    t.integer "quantity"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "rates", force: :cascade do |t|
     t.integer "star"
     t.string "comment"
@@ -38,4 +51,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_18_001130) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "gender"
+    t.string "email"
+    t.string "password"
+    t.string "role"
+    t.bigint "address_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["address_id"], name: "index_users_on_address_id"
+  end
+
+  add_foreign_key "products", "categories"
+  add_foreign_key "users", "addresses"
 end
