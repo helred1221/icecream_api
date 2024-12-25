e50e0a0e886be851fcd0669d1b4c7145a27da785
Helder45 <helder.pereira@estudante.ifms.edu.br>
2024-12-25 15:17:26 -0400

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_25_164411) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_25_170935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_25_164411) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sales", force: :cascade do |t|
+    t.integer "amount"
+    t.datetime "sale_date", precision: nil
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sales_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.boolean "gender"
@@ -64,5 +73,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_25_164411) do
   end
 
   add_foreign_key "products", "categories"
+  add_foreign_key "sales", "users"
   add_foreign_key "users", "addresses"
 end
